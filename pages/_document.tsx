6ca90290ee6d6fd58aefc1b88ae9ecaca93f4db4
Tournoi Teenbi Admin <tournoi@teenbi.local>
2026-05-26 02:05:58 +0000

    Add Teenbi icon + update manifest and document meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,14 @@ class MyDocument extends Document {
       <Html lang="fr">
         <Head>
           <meta charSet="utf-8" />
-          <meta name="theme-color" content="#0f0f0f" />
+          <meta name="theme-color" content="#f5c518" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-title" content="Teenbi" />
+          <link rel="icon" type="image/jpeg" href="/icon-teenbi.jpg" />
+          <link rel="apple-touch-icon" href="/icon-teenbi.jpg" />
+          <link rel="apple-touch-startup-image" href="/icon-teenbi.jpg" />
+          <link rel="manifest" href="/manifest.webmanifest" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
